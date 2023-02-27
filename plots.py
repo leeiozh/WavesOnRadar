05a13eab0a5e2f13baa3852efd74d589500c7ece
ezhova.ea <ezhova.ea@phystech.edu>
2023-02-27 08:48:24 +0300

    replace plots.py

--- plots.py
+++ plots.py
@@ -8,6 +8,8 @@ def func(x, A, B):
     return A + B * np.sqrt(x)
 
 
+PATH = '/storage/kubrick/ezhova/WavesOnRadar/'
+
 fig, axs = plt.subplots(1, 1, figsize=(4, 4), facecolor='w', edgecolor='k')
 fig.subplots_adjust(hspace=.3, wspace=.3)
 
@@ -31,21 +33,20 @@ mask[-12:] = np.zeros(12)
 # mask[-5] = 1
 
 
-
-#axs.scatter(df["buoy_swh"], func(df["radar_m0"], *popt), edgecolor='black', s=70)
+# axs.scatter(df["buoy_swh"], func(df["radar_m0"], *popt), edgecolor='black', s=70)
 ## axs.scatter(df["buoy_swh"] * (1 - mask), func(df["radar_m0"] * (1 - mask), *popt), edgecolor='black', s=70,
 ##            label='noisy data')
-#axs.legend()
-#axs.plot([0., 5], [0., 5], color='black')
+# axs.legend()
+# axs.plot([0., 5], [0., 5], color='black')
 #
-#axs.text(3, 1, 'corr =' + str(round(np.corrcoef(df["buoy_swh"], func(df["radar_m0"], *popt))[0, 1], 2)))
-#axs.text(3, 1.2,
+# axs.text(3, 1, 'corr =' + str(round(np.corrcoef(df["buoy_swh"], func(df["radar_m0"], *popt))[0, 1], 2)))
+# axs.text(3, 1.2,
 #         'rmse =' + str(round(np.linalg.norm(df["buoy_swh"] - func(df["radar_m0"], *popt)) / np.sqrt(df.shape[0]), 2)))
 #
-#axs.set_xlabel("SWH by BUOY, s")
-#axs.set_ylabel("SWH by RADAR, s")
-#axs.set_xlim(0., 4.5)
-#axs.set_ylim(0., 4.5)
+# axs.set_xlabel("SWH by BUOY, s")
+# axs.set_ylabel("SWH by RADAR, s")
+# axs.set_xlim(0., 4.5)
+# axs.set_ylim(0., 4.5)
 #
 tmp_an = df["radar_an"].to_numpy()
 mask = np.where(np.abs(df["buoy_ang"] - df["radar_an"]) > 180)
@@ -77,5 +78,5 @@ axs.text(20, 250, 'rmse =' + str(round(np.linalg.norm(df["buoy_ang"] - tmp_an) /
 #          'rmse =' + str(round(np.linalg.norm(df["buoy_per"] - df["radar_per"]) / np.sqrt(df["radar_per"].shape[0]), 2)))
 
 axs.grid(linestyle=':')
-plt.savefig('/storage/kubrick/ezhova/WavesOnRadar/plots/dir9.png', bbox_inches='tight', dpi=1000)
+plt.savefig(PATH + 'plots/dir9.png', bbox_inches='tight', dpi=1000)
 plt.show()
